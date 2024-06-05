354f47acae84798f89630484d0b28c4adecd8e73
Oleg Sovetnik <5460740+sovetnik@users.noreply.github.com>
2024-06-05 06:38:07 +0300

    Use name given for dump as root directory (#3)

--- test/mix/tasks/dump_test.exs
+++ test/mix/tasks/dump_test.exs
@@ -3,23 +3,25 @@ defmodule Mix.Tasks.DumpTest do
 
   alias Mix.Tasks.Dump
 
-  test "dump parsed ast in umwelt.bin by default" do
-    assert :ok = Dump.run([])
+  describe "parse([root_name])" do
+    test "parse app from Mix.Project.config by default" do
+      assert :ok = Dump.run([])
 
-    {:ok, bin} = File.read("umwelt.bin")
+      {:ok, bin} = File.read("umwelt.bin")
 
-    assert %{} = :erlang.binary_to_term(bin)
+      assert %{} = :erlang.binary_to_term(bin)
 
-    File.rm("umwelt.bin")
-  end
+      File.rm("umwelt.bin")
+    end
 
-  test "dump parsed ast into given filename" do
-    assert :ok = Dump.run(["dump.bin"])
+    test "parse app from given root_name" do
+      assert :ok = Dump.run(["umwelt"])
 
-    {:ok, bin} = File.read("dump.bin")
+      {:ok, bin} = File.read("umwelt.bin")
 
-    assert %{} = :erlang.binary_to_term(bin)
+      assert %{} = :erlang.binary_to_term(bin)
 
-    File.rm("dump.bin")
+      File.rm("umwelt.bin")
+    end
   end
 end
