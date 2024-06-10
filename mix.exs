8a756b758525dbb4c850c5ddafae0a6de7faade0
Oleg Sovetnik <5460740+sovetnik@users.noreply.github.com>
2024-06-10 05:07:35 +0300

    Parse fields without values in defstruct (#4)
    * Parse fields without values in defstruct
    
    * Update version to 0.1.3

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Umwelt.MixProject do
   def project do
     [
       app: :umwelt,
-      version: "0.1.2",
+      version: "0.1.3",
       elixir: "~> 1.15",
       compilers: [:leex, :yecc] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
